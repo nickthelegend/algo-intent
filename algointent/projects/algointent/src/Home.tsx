ed970d661d58f5dbf0969367a5833e917a4eea33
Manav Goyal <manucool123.rich@gmail.com>
2025-06-29 23:07:43 +0530

    Update for phone design

--- algointent/projects/algointent/src/Home.tsx
+++ algointent/projects/algointent/src/Home.tsx
@@ -7,16 +7,8 @@ import logo from './assets/logo.svg';
 const Home: React.FC = () => {
   return (
     <div className="main-container min-h-screen flex flex-col bg-[#f8fafc]" style={{ minHeight: '100vh', width: '100vw' }}>
-      {/* Logo at top left */}
-      <div style={{ position: 'fixed', top: '2rem', left: '2rem', zIndex: 201 }}>
-        <a href="/">
-          <img src={logo} alt="Algo-Intent Logo" style={{ width: 40, height: 40, borderRadius: '1rem' }} />
-        </a>
-      </div>
-      {/* Wallet connect button at top right */}
-      <div style={{ position: 'fixed', top: '2rem', right: '2rem', zIndex: 200 }}>
-        <WalletConnectButton />
-      </div>
+      {/* Spacer for wallet button on mobile */}
+      <div className="header-spacer"></div>
       {/* Header/title centered at the top */}
       <div style={{ width: '100%', textAlign: 'center', marginTop: '5rem', marginBottom: '2.5rem' }}>
         <h1 style={{ fontWeight: 800, fontSize: '2.3rem', letterSpacing: '-1px', color: '#1a232b', margin: 0 }}>Algo-Intent Chat</h1>
@@ -27,6 +19,10 @@ const Home: React.FC = () => {
         <div style={{ width: '100%', maxWidth: 720, margin: '0 auto', flex: 1, display: 'flex', flexDirection: 'column', justifyContent: 'flex-end' }}>
           <AlgoIntentChat />
         </div>
+      </div>
+      {/* Wallet connect button at top right (all screens) */}
+      <div className="wallet-connect-top-right">
+        <WalletConnectButton />
       </div>
     </div>
   );
